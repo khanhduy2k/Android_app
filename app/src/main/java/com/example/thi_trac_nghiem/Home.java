dda559253b83c4e0b711b492e3de359bed02645d
duykhanh <zerok49tt@gmail.com>
2021-05-12 16:25:23 +0700

    add event onClick in Home

--- app/src/main/java/com/example/thi_trac_nghiem/Home.java
+++ app/src/main/java/com/example/thi_trac_nghiem/Home.java
@@ -1,14 +1,56 @@
 package com.example.thi_trac_nghiem;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class Home extends AppCompatActivity {
 
+//  Khai báo button
+    ConstraintLayout
+            btnThiThu,
+            btnCauDiemLiet,
+            btnHocBienBao,
+            btnCauSai,
+            btnThiSaHinh,
+            btnMeoThi;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        goibutton();
+
     }
+
+//  Ánh xạ button
+    public void goibutton() {
+        btnThiThu = (ConstraintLayout) findViewById(R.id.btnBatDauThi);
+        btnCauDiemLiet = (ConstraintLayout) findViewById(R.id.btnCauDiemLiet);
+        btnHocBienBao = (ConstraintLayout) findViewById(R.id.btnCacBienBao);
+        btnCauSai = (ConstraintLayout) findViewById(R.id.btnCauSai);
+        btnThiSaHinh = (ConstraintLayout) findViewById(R.id.btnThiSaHinh);
+        btnMeoThi = (ConstraintLayout) findViewById(R.id.btnMeoThi);
+    }
+
+//  Hàm sự kiện Click
+    public void onClick(View v) {
+        if (v == btnThiThu) {
+            Toast.makeText(Home.this, "Bắt đầu thi", Toast.LENGTH_LONG).show();
+        }else if (v == btnCauDiemLiet) {
+            Toast.makeText(Home.this, "Phần câu diểm liệt", Toast.LENGTH_LONG).show();
+        }else if (v == btnCauSai) {
+            Toast.makeText(Home.this, "Phần câi trả lời sai", Toast.LENGTH_LONG).show();
+        }else if (v == btnHocBienBao) {
+            Toast.makeText(Home.this, "Phần học biển báo", Toast.LENGTH_LONG).show();
+        }else if (v == btnThiSaHinh) {
+            Toast.makeText(Home.this, "Phần thi sa hình", Toast.LENGTH_LONG).show();
+        }else if (v == btnMeoThi) {
+            Toast.makeText(Home.this, "Phần mẹo thi", Toast.LENGTH_LONG).show();
+        }
+    }
+
 }
--- app/src/main/java/com/example/thi_trac_nghiem/MainActivity.java
+++ app/src/main/java/com/example/thi_trac_nghiem/MainActivity.java
@@ -4,9 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private static int SPLASH_SCREEN = 2500;
@@ -35,4 +37,5 @@ public class MainActivity extends AppCompatActivity {
             }
         }, SPLASH_SCREEN);
     }
+
 }
