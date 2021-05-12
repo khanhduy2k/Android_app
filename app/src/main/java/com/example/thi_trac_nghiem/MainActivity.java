0a515840575ff0d8488e724f67213ca3dfcf88e8
duykhanh <zerok49tt@gmail.com>
2021-05-12 09:41:54 +0700

    add animation MainActivity

--- app/src/main/java/com/example/thi_trac_nghiem/MainActivity.java
+++ app/src/main/java/com/example/thi_trac_nghiem/MainActivity.java
@@ -4,20 +4,33 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private static int SPLASH_SCREEN = 2500;
+
+    Animation animAlpha;
+    TextView logo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
 
+        animAlpha = AnimationUtils.loadAnimation(MainActivity.this, R.anim.anim_alpha);
+
+        logo = findViewById(R.id.logo);
+
+        logo.setAnimation(animAlpha);
+
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
                 Intent intent = new Intent(MainActivity.this, Home.class);
                     startActivity(intent);
+                    overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
                     finish();
             }
         }, SPLASH_SCREEN);
